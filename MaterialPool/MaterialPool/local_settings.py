f5068d6674d88ca8bb3fa51c5600e794a23c0654
arundas <arundas1903@gmail.com>
2015-07-01 20:56:20 +0530

    Initial setup of project

---
+++ MaterialPool/MaterialPool/local_settings.py
@@ -0,0 +1,11 @@
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'material_pool',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
