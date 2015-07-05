5e4a0cda660556609da693154018023c15e0bf1f
arundas <arundas1903@gmail.com>
2015-07-05 13:24:00 +0530

    Small changes

--- MaterialPool/auth_user/models.py
+++ MaterialPool/auth_user/models.py
@@ -3,4 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class PoolUser(AbstractUser):
+
+    """ Model for Material Pool User """
+
     is_subscribed = models.BooleanField(default=False)
--- MaterialPool/auth_user/views.py
+++ MaterialPool/auth_user/views.py
@@ -2,8 +2,9 @@ from datetime import datetime, timedelta
 import pytz
 
 from django.shortcuts import get_object_or_404
-from django.contrib.auth import login, logout
-from rest_framework import mixins, generics, renderers, permissions, status
+from django.contrib.auth import login
+from django.http import Http404
+from rest_framework import generics, renderers, permissions, status
 from rest_framework.response import Response
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.models import Token
@@ -48,15 +49,15 @@ class CreateUser(generics.GenericAPIView):
 
 class UserLogin(generics.GenericAPIView):
 
-    """ Api for Login """
+    """ Api for Login and Logout"""
 
     serializer_class = AuthTokenSerializer
     renderer_classes = (renderers.JSONRenderer, )
 
     def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.DATA)
+        data = request.DATA
+        serializer = self.serializer_class(data=data)
         if serializer.is_valid():
-            previous_login = serializer.validated_data['user'].last_login
             login(request, serializer.validated_data['user'])
             token, created = Token.objects.get_or_create(
                 user=serializer.validated_data['user'])
@@ -67,12 +68,64 @@ class UserLogin(generics.GenericAPIView):
                     token, created = Token.objects.get_or_create(
                         user=serializer.validated_data['user'])
             user = request.user
-            user.previous_login = previous_login
             user.save()
             return Response({'status': 1,
                              'token': token.key,
                              'first_name': request.user.first_name,
                              'last_name': request.user.last_name,
                              'username': request.user.username})
+        try:
+            user = get_object_or_404(PoolUser, username=data['username'])
+            password = user.check_password(data['password'])
+            if not password:
+                return Response({'status': -1, 'errors': 'Invalid Password'})
+        except:
+            return Response({'status': -1, 'errors': 'Invalid Username'})
         return Response({'status': -1, "error": serializer.errors},
                         status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, *args, **kwargs):
+        token = request.META.get('HTTP_AUTHORIZATION', None)
+        if token:
+            try:
+                token_obj = get_object_or_404(Token, key=token.split()[1])
+                token_obj.delete()
+            except Http404:
+                pass
+        return Response({'status': 1, 'token': None})
+
+
+class ChangePassword(generics.GenericAPIView):
+
+    """Api to change password"""
+
+    renderer_classes = (renderers.JSONRenderer,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        password = request.data.get("new_password")
+        confirm_password = request.data.get("confirm_password")
+        if password and confirm_password:
+            pass
+        else:
+            return Response(
+                {
+                    'status': -1,
+                    'errors': 'Please enter password to be changed'
+                }
+            )
+        if password == confirm_password:
+            user = request.user
+            if user.check_password(password):
+                return Response(
+                    {
+                        "status": -1,
+                        "errors": "Please enter a new password to change"}
+                )
+            user.set_password(password)
+            user.save()
+            return Response({"status": 1})
+        else:
+            return Response(
+                {"status": -1, "errors": "Passwords did not match"}
+            )
